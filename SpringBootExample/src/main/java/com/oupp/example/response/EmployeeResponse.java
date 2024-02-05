42de64b408115e0e19ba0ad361e0d6450cf655a0
asishkumargoudaoupp <156104885+asishkumargoudaoupp@users.noreply.github.com>
2024-02-05 10:10:31 +0530

    Add files via upload
    Sample Spring Boot Example Application

---
+++ SpringBootExample/src/main/java/com/oupp/example/response/EmployeeResponse.java
@@ -0,0 +1,39 @@
+package com.oupp.example.response;
+
+public class EmployeeResponse {
+	
+
+	private int eid;
+	private String name;
+	private String email;
+	private String designation;
+	
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+
+}
