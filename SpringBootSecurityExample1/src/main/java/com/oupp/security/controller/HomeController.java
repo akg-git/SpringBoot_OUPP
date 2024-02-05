4b6959ae770e318f94dd3632a3793b799a23bb08
asishkumargoudaoupp <156104885+asishkumargoudaoupp@users.noreply.github.com>
2024-02-05 10:16:15 +0530

    Add files via upload

---
+++ SpringBootSecurityExample1/src/main/java/com/oupp/security/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.oupp.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+
+}
