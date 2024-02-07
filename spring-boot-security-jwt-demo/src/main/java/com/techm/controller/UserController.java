e98688278626e47acac279df9b422e43cfe727f6
asishkumargoudaoupp <156104885+asishkumargoudaoupp@users.noreply.github.com>
2024-02-07 11:09:34 +0530

    Add files via upload
    Spring Boot Security JWT Demo Project Commit 1

---
+++ spring-boot-security-jwt-demo/src/main/java/com/techm/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.techm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techm.entity.UserInfo;
+import com.techm.request.AuthRequest;
+import com.techm.service.JwtService;
+import com.techm.service.UserInfoService;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+	@Autowired
+	private UserInfoService service;
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome this endpoint is not secure";
+	}
+
+	@PostMapping("/addNewUser")
+	public String addNewUser(@RequestBody UserInfo user) {
+		return service.addUser(user);
+	}
+
+	@GetMapping("/user/userProfile")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public String userProfile() {
+		return "Welcome user";
+	}
+	
+	@GetMapping("/admin/adminProfile")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public String adminProfile() {
+		return "Welcome admin";
+	}
+	
+	@PostMapping("/generateToken")
+	public String AuthenticateAndGetToken(@RequestBody AuthRequest request) {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(request.getName(), request.getPassword()));
+		if(authentication.isAuthenticated()) {
+			return jwtService.generateToken(request.getName());
+		}else {
+			throw new UsernameNotFoundException("Invalid User Request.");
+		}
+	}
+}
---
+++ spring-boot-security-jwt-demo/src/main/java/com/techm/service/UserInfoService.java
@@ -0,0 +1,39 @@
+package com.techm.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.techm.entity.UserInfo;
+import com.techm.repository.UserInfoRepository;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+
+	@Autowired
+	private UserInfoRepository repository;
+
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<UserInfo> user = repository.findByName(username);
+
+		// Convert user to USerDetails
+		return user.map(UserInfoDetails::new)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found" + username));
+	}
+	
+	public String addUser(UserInfo user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		repository.save(user);
+		return "User added successfully" + user.getName();
+	}
+
+}
